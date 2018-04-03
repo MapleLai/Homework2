948d87f08d8471e19cccb1a7955a4882d3a88215
MapleLai <37803093+MapleLai@users.noreply.github.com>
2018-04-04 02:39:19 +0800

    Add files via upload

---
+++ 牧师与魔鬼/Controler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using myGame;
+
+namespace myGame{
+	public class SSDirector: System.Object{
+		
+		private static SSDirector _instance;
+		private Model _model;
+
+		public static SSDirector getInstance(){
+			if (_instance == null)
+				_instance = new SSDirector ();
+			return _instance;
+		}
+
+		public Model getModel(){
+			return _model;
+		}
+
+		internal void setModel(Model model){
+			_model = model;
+		}
+
+		public void moveBoat(){
+			_model.moveBoat ();
+		}
+
+		public void onBoat(GameObject gameObject){
+			_model.onBoat (gameObject);
+		}
+
+		public void offBoat(GameObject gameObject){
+			_model.offBoat (gameObject);
+		}
+			
+		public int check(){
+			return _model.check ();
+		}
+	}
+}
+
+public class Controler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+}
